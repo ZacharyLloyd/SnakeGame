2d0995dfcbbc4263876314e36256532cd7d7b2be
Tyrease Teer <kayaneseera@gmail.com>
2019-10-21 18:04:58 -0700

    Remade Snake Game in QT

---
+++ SnakeGameInQT/SnakeGame/gameloop.h
@@ -0,0 +1,12 @@
+#ifndef GAMELOOP_H
+#define GAMELOOP_H
+
+
+class gameloop
+{
+public:
+    gameloop();
+    void Initiate();
+};
+
+#endif // GAMELOOP_H
--- SnakeGameInQT/SnakeGame/main.cpp
+++ SnakeGameInQT/SnakeGame/main.cpp
@@ -4,16 +4,6 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    Board * gameBoard = new Board();
-
-    gameBoard->Setup();
-
-    while(!gameBoard->gameOver){
-        gameBoard->Setup();
-        gameBoard->Draw();
-    }
-
-    gameBoard->~Board();
 
     return a.exec();
 }
