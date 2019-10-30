8672a2309eebc89f27bfe7a538398d01937252b8
Tyrease Teer <kayaneseera@gmail.com>
2019-10-29 19:14:41 -0700

    Tried things... didn't work.
    Tried helping the linker do it's job by creating a new solution (located in SnakeGameDos), and it didn't work.

--- SnakeGame/SnakeGame/Direction.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-enum class Direction { STOP = 0, LEFT = 1, RIGHT = 2, UP = 3, DOWN = 4 };
--- SnakeGame/SnakeGame/Snake.cpp
+++
@@ -1,9 +0,0 @@
-#include "Snake.h"
-#include <iostream>
-
-using namespace std;
-
-void Snake::CreateSnake()
-{
-	cout << "O";
-}
